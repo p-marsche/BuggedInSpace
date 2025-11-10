6978fb66e2cac0c0356d070bcb414e5a8da139a2
Pascal Veselka <fhs52324@fh-salzburg.ac.at>
2025-11-11 00:29:19 +0100

    added config file and replaced paths in AssetManager with the config syntax

---
+++ Engine_WS25/Engine_WS25/Config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+struct Config
+{
+	static const std::string assetBasePath;
+	static const std::string texturesPath;
+	static const std::string soundsPath;
+	static const std::string fontsPath;
+	static const std::string musicPath;
+	static const std::string imagesPath;
+	static const std::string spriteSheetPath;
+};
