51e921e3d32d988ec2afbff638bd2a0544abaaab
Pascal Veselka <fhs52324@fh-salzburg.ac.at>
2025-11-10 11:57:31 +0100

    added GameStateEnum

--- Engine_WS25/Engine_WS25/GameState.hpp
+++ Engine_WS25/Engine_WS25/GameState.hpp
@@ -7,7 +7,7 @@ class GameState
 public:
 	virtual void init(GameStateManager* manager);
 	virtual void exit() = 0;
-	virtual void update(float delta) = 0;
+	virtual void update(float deltaTime) = 0;
 	virtual void render() = 0;
 
 private:
---
+++ Engine_WS25/Engine_WS25/GameStateEnum.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+enum class GameStateEnum
+{
+	Game,
+	Exit
+};
