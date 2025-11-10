c1fda5f129e5f11f6b1b1b59bf7aedbdf9928e9a
Pascal Marsche <pmarsche.mmt-b2024@fh-salzburg.ac.at>
2025-11-10 11:11:24 +0100

    2 player movement working (WASD/ArrowKeys)

--- Engine_WS25/Engine_WS25/GameObject.cpp
+++ Engine_WS25/Engine_WS25/GameObject.cpp
@@ -8,6 +8,11 @@ GameObject::GameObject()
 	: m_ID(s_nextID++)
 { }
 
+void GameObject::moveObject(sf::Vector2f moveVec)
+{
+	this->move(moveVec);
+}
+
 void GameObject::update(float deltaTime)
 {
 	for (auto& [ctype, comp] : m_components)
--- Engine_WS25/Engine_WS25/GameObjectFactory.cpp
+++ Engine_WS25/Engine_WS25/GameObjectFactory.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 
+#include "AssetManager.hpp"
 #include "ComponentTypeEnum.hpp"
 #include "GameObjectFactory.hpp"
 
@@ -12,19 +13,29 @@ GameObjectFactory& GameObjectFactory::getInstance()
 std::unique_ptr<GameObject> GameObjectFactory::createPlayer(int playerNumber)
 {
 	if ((playerNumber != 1) && (playerNumber != 2))
+		//add exception here later?
 		return nullptr;
 
 	auto player = std::make_unique<GameObject>();
-	std::string filename = "Player1_Ship.png";
-	std::unique_ptr<sf::Texture> player1Tex = std::make_unique<sf::Texture>();
-	player1Tex->loadFromFile("../Engine_WS25/Assets/Textures/" + filename);
+	std::string filename;
+	std::string key;
 
-	/*std::shared_ptr<RenderComponent> render = (playerNumber == 1) ?
-		ComponentFactory::GetInstance().CreateRenderComponent(--insert player1 asset here--) :
-		ComponentFactory::GetInstance().CreateRenderComponent(--insert player2 asset here--);*/
+	if (playerNumber == 1)
+	{
+		key = "Player1";
+		filename = "Player_Ship1.png";
+	}
+	else
+	{
+		key = "Player2";
+		filename = "Player_Ship2.png";
+	}
+
+	AssetManager::getInstance().loadTexture(key, filename);
+	sf::Texture& player1Tex = AssetManager::getInstance().getTexture(key);
 	std::shared_ptr<RenderComponent> render =
-		ComponentFactory::getInstance().createRenderComponent(*player1Tex);
+		ComponentFactory::getInstance().createRenderComponent(player1Tex);
 	player->addComponent(ComponentType::Render, render);
-	player->addComponent(ComponentType::Input, ComponentFactory::getInstance().createInputComponent(1));
+	player->addComponent(ComponentType::Input, ComponentFactory::getInstance().createInputComponent(playerNumber));
 	return player;
 }
