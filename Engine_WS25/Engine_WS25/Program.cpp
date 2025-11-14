765d9c29c64d0a8ad36822fff7426b9ab0c4ca27
Pascal Marsche <pmarsche.mmt-b2024@fh-salzburg.ac.at>
2025-11-14 21:00:38 +0100

    minor tweaks

--- Engine_WS25/Engine_WS25/Program.cpp
+++ Engine_WS25/Engine_WS25/Program.cpp
@@ -13,23 +13,12 @@ int main()
 {
 	try
 	{
-		//InputManager::getInstance().init();
-
-		std::cout << "Press ESC key to close window" << std::endl;
-		// disabled for now cause causes memory leak
-		/*gameStateManager.registerState(GameStateEnum::Game, new Game());
-		gameStateManager.registerState(GameStateEnum::Menu, new TestMenu());
-		gameStateManager.setState(GameStateEnum::Menu);*/
-		//instead just run Game
 		Game game = Game();
 		game.run();
-		std::cout << "All done" << std::endl;
 	}
 	catch (std::exception e)
 	{
 		ErrorWindow errorWindow = ErrorWindow(e.what());
 		errorWindow.showError();
 	}
-
-	return 0; // only here to shut compiler up for now :D
 };
